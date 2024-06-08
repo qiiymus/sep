6acc84edad76dad298482a932e0cf2ec1eb057be
Tisyalene Thangarajoo <tisyalenetisya05@gmail.com>
2024-06-08 22:08:04 +0800

    Create KAChooseClassPage.php

---
+++ resources/views/ManageStudentResults/KAChooseClassPage.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="bg-[#FFFFFF] relative">
+      <div
+        class="bg-[#9FB996] shadow-[_0px_4px_4px_0px_rgba(0,0,0,0.25)] absolute"
+        style="left: 0px; width: 105px; top: 0px; height: 1024px"></div>
+      <img
+        class="absolute"
+        style="left: 32px; width: 40px; top: 449px; height: 40px; object-fit: cover"
+        src="https://placehold.co/256" />
+      <img
+        class="absolute"
+        style="left: 32px; width: 40px; top: 622px; height: 40px; object-fit: cover"
+        src="https://placehold.co/256" />
+      <img
+        class="absolute"
+        style="left: 32px; width: 40px; top: 277px; height: 40px; object-fit: cover"
+        src="https://placehold.co/256" />
+      <img
+        class="absolute"
+        style="left: 36px; width: 40px; top: 707px; height: 40px; object-fit: cover"
+        src="https://placehold.co/256" />
+      <img
+        class="absolute"
+        style="left: 32px; width: 40px; top: 537px; height: 40px; object-fit: cover"
+        src="https://placehold.co/256" />
+      <div
+        class="text-[#2D532D] text-[64px] font-semibold absolute"
+        style="left: 150px; width: 1022px; top: 60px; height: 174px">
+        Semak Keputusan Pelajar
+      </div>
+      <div
+        class="text-[#000000] text-[40px] font-semibold absolute"
+        style="left: 150px; width: 736px; top: 204px; height: 73px">
+        Sila pilih kelas pelajar
+      </div>
+      <div
+        class="bg-[#D9D9D9] border-solid border-[#000000] border rounded-[10px] absolute"
+        style="left: 150px; width: 640px; top: 317px; height: 70px"></div>
+      <div
+        class="text-[#000000] text-[40px] absolute"
+        style="left: 329px; width: 241px; top: 319px; height: 62px">
+        -Sila pilih-
+      </div>
+      <img
+        class="absolute"
+        style="left: 32px; width: 40px; top: 364px; height: 40px; object-fit: cover"
+        src="https://placehold.co/256" />
+      <svg
+        width="72"
+        height="69"
+        viewBox="0 0 72 69"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M21 28.75L36 43.125L51 28.75H21Z" fill="black" />
+      </svg>
+      <div
+        class="bg-[#547947] border-solid border-[#000000] border rounded-[10px] absolute"
+        style="left: 570px; width: 228px; top: 449px; height: 63px"></div>
+      <div
+        class="text-[#FFFFFF] text-[25px] font-semibold absolute"
+        style="left: 623px; width: 162px; top: 458px; height: 31px">
+        Teruskan
+      </div>
+      <img
+        class="absolute"
+        style="left: 25px; width: 54px; top: 70px; height: 72px; object-fit: cover"
+        src="https://placehold.co/256" />
+    </div>
+  </body>
+</html>
