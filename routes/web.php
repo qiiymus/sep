cf18e4f21a7654f2f66f58b4e6b4ef10c3e5458f
Tisyalene Thangarajoo <tisyalenetisya05@gmail.com>
2024-06-09 03:14:56 +0800

    result controller

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\ResultController;
+
+Route::get('/ka-choose-class', [ResultController::class, 'showKAChooseClassPage'])->name('ka.choose.class');
