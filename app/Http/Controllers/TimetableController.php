dfe8c552983c81e827038cef1181c5bae2b6c45e
qiiymus <nqmus1306@gmail.com>
2024-06-13 00:08:41 +0800

    13/6

--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -10,57 +10,63 @@ use Illuminate\View\View;
 
 class TimetableController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function ParentViewTT() {
+        return view('ManageKAFATimetable.ParentsTimetablePage');
+    }
+
+    public function TeacherViewTT() {
+        return view('ManageKAFATimetable.TeacherTimetablePage');
+    }
+
+    public function KAViewTT() {
+        return view('ManageKAFATimetable.AdminTimetablePage');
+    }
+
+    //Display a listing of the resource.
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    //Show the form for creating a new resource.
+    
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    //Store a newly created resource in storage.
+    
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+    
+    //Display the specified resource.
+    
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
+    //Show the form for editing the specified resource.
+    
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    //Update the specified resource in storage.
+    
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    //Remove the specified resource from storage.
+    
     public function destroy(string $id)
     {
         //
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\TimetableController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ManageKAFATimetable', [TimetableController::class], 'ParentsViewTT');
+Route::get('/ManageKAFATimetable', [TimetableController::class], 'TeacherViewTT');
+Route::get('/ManageKAFATimetable', [TimetableController::class], 'KAViewTT');
