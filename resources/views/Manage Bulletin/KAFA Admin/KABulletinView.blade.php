62093843a772f6934fd72df5402eb347dfff42e1
Zeyynn <134064848+Zeyynn@users.noreply.github.com>
2024-06-14 00:16:39 +0800

    afgsahrssggjfjfhdgrhdtd

--- resources/views/Manage Bulletin/KAFA Admin/KABulletinView.blade.php
+++ resources/views/Manage Bulletin/KAFA Admin/KABulletinView.blade.php
@@ -11,15 +11,15 @@
     <h1>KAFA Bulletin</h1>
 </div>
   <div class="card-body">
-    <h2>Butiran Bulletin</h2>
+    <h2>Butiran Bulletin</h2><hr>
         <div class="card-body">
-          <p class="card-title">Daripada : {{ $bulletin->daripada }}</p>
-          <p class="card-title">Kepada : {{ $bulletin->kepada }}</p><hr>
-          <h3 class="card-title">Tajuk : {{ $bulletin->tajuk }}</h3><hr>
-          <p class="card-text">Tarikh : {{ $bulletin->tarikh }}</p><hr>
-          <p class="card-text">Kepentingan : {{ $bulletin->kepentingan }}</p><hr>
-          <p class="card-text">Status : {{ $bulletin->status }}</p><hr>
-          <p class="card-text">Isi : {{ $bulletin->isi }}</p><hr>
+          <p class="card-title">Daripada : {{ $bulletins->daripada }}</p>
+          <p class="card-title">Kepada : {{ $bulletins->kepada }}</p><hr>
+          <h3 class="card-title">Tajuk : {{ $bulletins->tajuk }}</h3><hr>
+          <p class="card-text">Tarikh : {{ $bulletins->tarikh }}</p>
+          <p class="card-text">Kepentingan : {{ $bulletins->kepentingan }}</p>
+          <p class="card-text">Status : {{ $bulletins->status }}</p><hr>
+          <p class="card-text">Isi : {{ $bulletins->isi }}</p><hr>
   </div>
     </hr>
   </div>
--- resources/views/Manage Bulletin/Teacher/TeacherBulletinView.blade.php
+++ resources/views/Manage Bulletin/Teacher/TeacherBulletinView.blade.php
@@ -10,13 +10,13 @@
       <h1>KAFA Bulletin</h1>
     </div>
     <div class="card-body">
-    <h2>Butiran Bulletin</h2>
+    <h2>Butiran Bulletin</h2><hr>
         <div class="card-body">
         <p class="card-title">Daripada : {{ $bulletin->daripada }}</p>
         <p class="card-title">Kepada : {{ $bulletin->kepada }}</p><hr>
         <h3 class="card-title">Tajuk : {{ $bulletin->tajuk }}</h3><hr>
-        <p class="card-text">Tarikh : {{ $bulletin->tarikh }}</p><hr>
-        <p class="card-text">Kepentingan : {{ $bulletin->kepentingan }}</p><hr>
+        <p class="card-text">Tarikh : {{ $bulletin->tarikh }}</p>
+        <p class="card-text">Kepentingan : {{ $bulletin->kepentingan }}</p>
         <p class="card-text">Status : {{ $bulletin->status }}</p><hr>
         <p class="card-text">Isi : {{ $bulletin->isi }}</p><hr>
   </div>
