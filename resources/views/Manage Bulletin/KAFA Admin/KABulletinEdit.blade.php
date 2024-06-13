a55adf484c923ecc39398a01243762225fe4e6a3
Zeyynn <134064848+Zeyynn@users.noreply.github.com>
2024-06-13 16:24:20 +0800

    kill me

---
+++ resources/views/Manage Bulletin/KAFA Admin/KABulletinEdit.blade.php
@@ -0,0 +1,34 @@
+
+
+@extends('Manage Bulletin.layouts')
+@section('content')
+  
+<div class="card" style="margin:20px;">
+  <div class="card-header">Sunting Bulletin</div>
+  <div class="card-body">
+       
+      <form action="{{ url('bulletin/' .$bulletins->id) }}" method="post">
+        {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{$bulletins->id}}" id="id" />
+        <label>Daripada</label></br>
+        <input type="text" name="daripada" id="daripada" value="{{$bulletins->daripada}}" class="form-control"></br>
+        <label>Kepada</label></br>
+        <input type="text" name="kepada" id="kepada" value="{{$bulletins->kepada}}" class="form-control"></br>
+        <label>Tajuk</label></br>
+        <input type="text" name="tajuk" id="tajuk" value="{{$bulletins->tajuk}}" class="form-control"></br>
+        <label>Tarikh</label></br>
+        <input type="date" name="tarikh" id="tarikh" value="{{$bulletins->tarikh}}" class="form-control"></br>
+        <label>Kepentingan</label></br>
+        <input type="text" name="kepentingan" id="kepentingan" value="{{$bulletins->kepentingan}}" class="form-control"></br>
+        <label>Status</label></br>
+        <input type="text" name="status" id="status" value="{{$bulletins->status}}" class="form-control"></br>
+        <label>Isi</label></br>
+        <input type="text" name="isi" id="isi" value="{{$bulletins->isi}}" class="form-control"></br>
+        <input type="submit" value="Kemaskini" class="btn btn-success"></br>
+    </form>
+    
+  </div>
+</div>
+  
+@stop
--- resources/views/Manage Bulletin/Parents/ParentsBulletinList.blade.php
+++ resources/views/Manage Bulletin/Parents/ParentsBulletinList.blade.php
@@ -7,7 +7,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h1>KAFA Bulletin</h1>
+                        <h1>KAFA Bulletin Parents</h1>
                     </div>
                     <div class="card-body">
                         <h2>Senarai Bulletin</h2>
@@ -29,20 +29,15 @@
                                 @foreach($bulletins as $item)
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->Tajuk }}</td> <!-- Ensure the column name matches exactly -->
-                                        <td>{{ $item->Tarikh }}</td> <!-- Ensure the column name matches exactly -->
-                                        <td>{{ $item->Kepentingan }}</td> <!-- Ensure the column name matches exactly -->
-                                        <td>{{ $item->Status }}</td> <!-- Ensure the column name matches exactly -->
+                                        <td>{{ $item->tajuk }}</td> <!-- Ensure the column name matches exactly -->
+                                        <td>{{ $item->tarikh }}</td> <!-- Ensure the column name matches exactly -->
+                                        <td>{{ $item->kepentingan }}</td> <!-- Ensure the column name matches exactly -->
+                                        <td>{{ $item->status }}</td> <!-- Ensure the column name matches exactly -->
 
                                         <td>
-                                            <a href="{{ url('/bulletin/' . $item->id) }}" title="View Bulletin"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> Lihat</button></a>
-                                            <a href="{{ url('/bulletin/' . $item->id . '/edit') }}" title="Edit Bulletin"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Sunting</button></a>
-
-                                            <form method="POST" action="{{ url('/bulletin' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Bulletin" onclick="return confirm('Confirm delete?')"><i class="fa fa-trash-o" aria-hidden="true"></i> Buang</button>
-                                            </form>
+                                            <a href="{{ url('/viewbulletinParents/' . $item->id) }}" title="View Bulletin">
+                                                <button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> Lihat</button>
+                                            </a>
                                         </td>
                                     </tr>
                                 @endforeach
@@ -54,9 +49,6 @@
                 </div>
                 <br/>
                         <br/>
-                <a href="{{ url('/bulletin/create') }}" class="btn btn-success btn-sm" title="Add New Bulletin">
-                    Add New
-                </a>
             </div>
             
         </div>
