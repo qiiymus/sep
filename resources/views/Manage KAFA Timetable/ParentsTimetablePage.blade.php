6aabb431f088264e51c3e827dfaf47cf3147cff4
qiiymus <nqmus1306@gmail.com>
2024-06-14 10:59:47 +0800

    timetable correction

--- resources/views/Manage KAFA Timetable/ParentsTimetablePage.blade.php
+++ resources/views/Manage KAFA Timetable/ParentsTimetablePage.blade.php
@@ -21,7 +21,7 @@
         font-weight: bold;
     }
     .schedule {
-      margin-top: 20px;
+      margin-top: 10px;
     }
     table {
       width: 100%;
@@ -35,15 +35,24 @@
     .rehat-row {
       background-color: #fff6a0;
     }
+    .kelas-ustazah-container {
+      display: flex;
+      padding: 10px;
+      justify-content: space-between; /*Align items with space between */
+      margin-right: 400px;
+    }
   </style>
 </head>
+
 <body>
 <div class="content-wrapper">
   @include('include.parentmenu')
   <div class="main">
     <h1>Jadual Waktu Kelas</h1>
-    <h6><b>Kelas:</b></h6>
-    <h6><b>Ustazah:</b></h6>
+    <div class="kelas-ustazah-container">
+      <h5><b>Kelas: 2 Fatanah</b></h5>
+      <h5><b>Ustazah: Ustazah Faridah</b></h5>
+    </div>
     <section class="schedule">
       <table>
         <tr>
@@ -54,23 +63,24 @@
           <th><b>3.30-4.00 pm</b></th>
         </tr>
         <tr>
+        <tr>
           <td>Isnin</td>
-          <td>Subject 1</td>
-          <td>Subject 2</td>
+          <td>AL-QURAN</td>
+          <td>SIRAH</td>
           <td class="rehat-row" rowspan="3">R E H A T</td>
-          <td>Subject 3</td>
+          <td>AKIDAH</td></td>
         </tr>
         <tr>
           <td>Selasa</td>
-          <td>Subject 1</td>
-          <td>Subject 2</td>
-          <td>Subject 3</td>
+          <td>AL-QURAN</td>
+          <td>JAWI/KHAT</td>
+          <td>IBADAH</td>
         </tr>
         <tr>
           <td>Khamis</td>
-          <td>Subject 1</td>
-          <td>Subject 2</td>
-          <td>Subject 3</td>
+          <td>AL-QURAN</td>
+          <td>BAHASA ARAB</td>
+          <td>ADAB</td>
         </tr>
       </table>
     </section>
